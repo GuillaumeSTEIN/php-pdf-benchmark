fc2619e89322da31928b5603ee340e3638959655
Guillaume STEIN <guillaume.stein@gmail.com>
2024-03-12 17:54:45 +0100

    include php-puppeteer in tests

--- k6/loadtests.sh
+++ k6/loadtests.sh
@@ -15,6 +15,10 @@ echo '    php-dompdf' >> docker_stats.txt;
 docker run --rm -i --network="host" grafana/k6 run -e URL=http://localhost:3002 - <k6/sequential.js
 
 echo '' >> docker_stats.txt;
+echo '    php-puppeteer' >> docker_stats.txt;
+docker run --rm -i --network="host" grafana/k6 run -e URL=http://localhost:3003 - <k6/sequential.js
+
+echo '' >> docker_stats.txt;
 echo 'Running concurrent tests' >> docker_stats.txt;
 echo '------------------------' >> docker_stats.txt;
 
@@ -29,6 +33,10 @@ docker run --rm -i --network="host" grafana/k6 run -e URL=http://localhost:3000/
 echo '' >> docker_stats.txt;
 echo '    php-dompdf' >> docker_stats.txt;
 docker run --rm -i --network="host" grafana/k6 run -e URL=http://localhost:3002 - <k6/concurrent.js
+
+echo '' >> docker_stats.txt;
+echo '    php-puppeteer' >> docker_stats.txt;
+docker run --rm -i --network="host" grafana/k6 run -e URL=http://localhost:3003 - <k6/concurrent.js
 
 echo '' >> docker_stats.txt;
 echo '------------------------' >> docker_stats.txt;
